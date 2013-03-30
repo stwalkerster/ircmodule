4aa4ac0c73d870e094ad33c1d00ec46a25ff222b
Simon Walker <github@stwalkerster.co.uk>
2013-03-30 07:41:19 +0000

    readerbase and writerbase

---
+++ IrcClientWriterBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Helpmebot.Irc
+{
+    public abstract class IrcClientWriterBase
+    {
+        protected readonly StreamWriter _writer;
+        protected readonly IIrc _network;
+
+        protected IrcClientWriterBase(StreamWriter writer, IIrc network)
+        {
+            _network = network;
+            _writer = writer;
+        }
+
+        public abstract void WriteLine(string message);
+    }
+}
