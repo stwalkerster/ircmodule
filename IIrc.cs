6f0e26fd0cc5ae85f4b738b3c24862bc34677562
Simon Walker <github@stwalkerster.co.uk>
2013-03-30 07:20:43 +0000

    namespace fixes

--- IIrc.cs
+++ IIrc.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace helpmebot.Irc
+namespace Helpmebot.Irc
 {
     public interface IIrc
     {
--- IrcClient.cs
+++ IrcClient.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace helpmebot.Irc
+namespace Helpmebot.Irc
 {
     public class IrcClient : IIrc
     {
