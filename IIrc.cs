e93630e4defde781da4d9279f12d21d74324ec77
Simon Walker <github@stwalkerster.co.uk>
2013-03-30 06:59:35 +0000

    irc module etc

---
+++ IIrc.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace helpmebot.Irc
+{
+    interface IIrc
+    {
+
+    }
+}
---
+++ IrcClient.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace helpmebot.Irc
+{
+    class IrcClient
+    {
+        private readonly string _server;
+        private readonly short _port;
+        private string _nickname;
+        private readonly string _username;
+        private readonly string _realname;
+        private readonly string _password;
+
+        protected IrcClient()
+        {
+
+        }
+
+        public IrcClient(string server, short port = 6667, string nickname = "IrcUser", string username = "user", string realname = "An IRC Client", string password = null)
+        {
+            _server = server;
+            _port = port;
+            _nickname = nickname;
+            _realname = realname;
+            _username = username;
+            _password = password;
+        }
+
+
+    }
+}
